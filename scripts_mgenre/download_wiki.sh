523941f36d0f4d7eea9247418b2f6b3b5b04f5be
Nicola De Cao <ndecao@fb.com>
2021-03-31 14:23:00 +0100

    Update download_wiki.sh

--- scripts_mgenre/download_wiki.sh
+++ scripts_mgenre/download_wiki.sh
@@ -12,7 +12,7 @@ cd wikipedia
 
 for LANG in af am ar as az be bg bm bn br bs ca cs cy da de el en eo es et eu fa ff fi fr fy ga gd gl gn gu ha he hi hr ht hu hy id ig is it ja jv ka kg kk km kn ko ku ky la lg ln lo lt lv mg mk ml mn mr ms my ne nl no om or pa pl ps pt qu ro ru sa sd si sk sl so sq sr ss su sv sw ta te th ti tl tn tr uk ur uz vi wo xh yo zh
 do
-    wget http://wikipedia.c3sl.ufpr.br/trwikinews/20191001/${LANG}wikinews-20191001-pages-articles-multistream.xml.bz2
+    wget http://wikipedia.c3sl.ufpr.br/${LANG}wiki/20191001/${LANG}wiki-20191001-pages-articles-multistream.xml.bz2
 done
 
 for LANG in af am ar as az be bg bm bn br bs ca cs cy da de el en eo es et eu fa ff fi fr fy ga gd gl gn gu ha he hi hr ht hu hy id ig is it ja jv ka kg kk km kn ko ku ky la lg ln lo lt lv mg mk ml mn mr ms my ne nl no om or pa pl ps pt qu ro ru sa sd si sk sl so sq sr ss su sv sw ta te th ti tl tn tr uk ur uz vi wo xh yo zh
